f62c55f476af2cf1d30c87cadaecfc60b4df05fb
Dev-Jose7 <71296183+Dev-Jose7@users.noreply.github.com>
2024-08-20 07:26:46 -0500

    Tercera clase, git y operator

---
+++ Operator.java
@@ -0,0 +1,14 @@
+package javanotes;
+
+public class Operator {
+
+    public static void main(String[] args) {
+        //Aritmeticos (+,-,/,*,%);
+
+        int num1 = 50;
+        int num2 = 35;
+        int suma = num1 + num2;
+
+        System.out.println(suma);
+    }
+}
